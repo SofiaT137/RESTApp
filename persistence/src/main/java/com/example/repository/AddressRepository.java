0caf27105eb404d3531a49ffbf65f49c50966c0a
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-09-13 11:46:05 +0300

    Add JavaDocs to the interfaces from repository folder persistence layer

--- persistence/src/main/java/com/example/repository/AddressRepository.java
+++ persistence/src/main/java/com/example/repository/AddressRepository.java
@@ -5,8 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * The "AddressRepository" interface extends JpaRepository functionality for the Address entity
+ */
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
+    /**
+     * Method "findByCityAndStreetAndHouseNumberAndOfficeNumber" searches the "Address" entity by its city,
+     * street, houseNumber, officeNumber
+     * @param city String city
+     * @param street String street
+     * @param houseNumber Integer houseNumber
+     * @param officeNumber Integer officeNumber
+     * @return Optional of the "Address" entity
+     */
     Optional<Address> findByCityAndStreetAndHouseNumberAndOfficeNumber(String city, String street, Integer houseNumber,
                                                               Integer officeNumber);
 }
--- persistence/src/main/java/com/example/repository/UserRepository.java
+++ persistence/src/main/java/com/example/repository/UserRepository.java
@@ -5,7 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * The "UserRepository" interface extends JpaRepository functionality for the User entity
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Method "findUserByLogin" searches the "User" entity by its login
+     * @param login String login
+     * @return Optional of the "User" entity
+     */
     Optional<User> findUserByLogin(String login);
 }
